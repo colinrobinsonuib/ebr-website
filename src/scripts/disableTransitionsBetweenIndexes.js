5f772feb8f2b7ffc79205c49998867980cd51d89
colin <144122160+colinrobinsonuib@users.noreply.github.com>
2025-06-15 17:00:10 +0200

    add interactive shadows

--- src/scripts/disableTransitionsBetweenIndexes.js
+++ src/scripts/disableTransitionsBetweenIndexes.js
@@ -4,8 +4,12 @@ export function disableTransitionsBetweenIndexs(){
 			const pathparts = path.split('/');
 			if (pathparts.length < 3 ) return false;
 			if ( !Number.isNaN(Number.parseInt(pathparts[2])) ) return false;
-			return ['essays', 'articles', 'reviews', 'interviews', 'riposte'].includes(pathparts[1])
+			return 'publications' === pathparts[1];
 		}
+
+		console.log(isPost(event.from.pathname));
+		console.log(isPost(event.to.pathname));
+		console.log(event.to.pathname);
 
 		if( !isPost(event.from.pathname) && !isPost(event.to.pathname) ) {
 			document.querySelectorAll('img[data-astro-transition-scope]').forEach(el => {
